fc270b4f7fc50a3febaf05d320b884c13f5df483
Noa <nois97.noa@gmail.com>
2022-09-18 18:09:46 +0200

    Restructure car sim to be more in line with a gym env

--- car_sim.py
+++ car_sim.py
@@ -20,6 +20,94 @@ obs, reward, done, _ env.step()
 
 """
 
+class ParkingSimulator:
+    def __init__(self) -> None:
+        self.width, self.height = (720, 720)
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.font.init()
+        self.my_font = pygame.font.SysFont(None, 30)
+        self.fps = RollingAverage(100)
+        self.t_start = time.time()
+        self.draw_scan_lines = False
+
+
+        self.x = None
+        self.reset()
+        self.u1 = 0
+        self.u2 = 0
+        self.dt = 0.5
+        self.L = 80
+        self.car_width = self.L
+        self.car_height = 50
+        self.car_xs , self.car_ys = get_car_corners(self.x[0, 0], self.x[1, 0], self.x[2, 0], self.car_width, self.car_height)
+        self.car_xs.append(self.car_xs[0]), self.car_ys.append(self.car_ys[0]) # Make the polygon closed
+
+        wheel_width = 30
+        wheel_height = 10
+        self.wheel1 = Wheel(wheel_width, wheel_height, self.car_width - wheel_width/2 - 5, -self.car_height/2 + wheel_height/2 + 5)
+        self.wheel2 = Wheel(wheel_width, wheel_height, self.car_width - wheel_width/2 - 5, self.car_height/2 - wheel_height/2 - 5)
+        self.wheel3 = Wheel(wheel_width, wheel_height, wheel_width/2 + 5, self.car_height/2 - wheel_height/2 - 5)
+        self.wheel4 = Wheel(wheel_width, wheel_height, wheel_width/2 + 5, -self.car_height/2 + wheel_height/2 + 5)
+
+        self.pa = ParkingArea([100, 300, 300, 100, 100], [100, 100, 300, 300, 100])
+
+        self.map_xs = [-350, 350, 350, 50, 50, -100, -100, -350, -350]
+        self.map_ys  = [-350, -350, 350, 350, 100, 100, 350, 350, -350]
+
+    def reset(self):
+        # TODO Make it spawn at random locations
+        self.x = np.zeros((4, 1))
+    def step(self, u1, u2):
+        self.t_start = time.time()
+
+        # Update car state and check for collision
+        x_temp = model(self.x, u1, u2, self.dt, self.L)
+        car_xs_temp, car_ys_temp = get_car_corners(x_temp[0, 0], x_temp[1, 0], x_temp[2, 0], self.car_width, self.car_height)
+        car_xs_temp.append(car_xs_temp[0]), car_ys_temp.append(car_ys_temp[0]) # Make the polygon closed
+        if ([], []) == get_polygon_intersection_points(car_xs_temp, car_ys_temp, self.map_xs, self.map_ys):
+            self.x = x_temp
+            self.car_xs, self.car_ys = car_xs_temp, car_ys_temp
+
+        self.car_pa_overlap = get_car_parking_area_overlap(self.pa.xs, self.pa.ys, self.car_xs, self.car_ys)
+
+        x_ = self.x[0, 0]
+        y = self.x[1, 0]
+        theta = self.x[2, 0]
+
+        self.map_scan_res = scan_numba(361, theta, x_, y, self.map_xs, self.map_ys)
+        self.pa_scan_res = scan_numba(361, theta, x_, y, self.pa.xs, self.pa.ys)
+        clean_pa_scan_lines(self.pa_scan_res, self.map_scan_res)
+
+        # return obs, reward, done, unkown
+
+    def render(self):
+        self.screen.fill(WHITE)
+        self.pa.draw(self.screen)
+
+        x_ = self.x[0, 0]
+        y = self.x[1, 0]
+        theta = self.x[2, 0]
+        phi = self.x[3, 0]
+
+
+        # Draw stuff
+        draw_car(self.car_xs[:-1], self.car_ys[:-1], self.screen, self.width, self.height)
+        self.wheel1.draw(x_, y, theta, phi, self.screen)
+        self.wheel2.draw(x_, y, theta, phi, self.screen)
+        self.wheel3.draw(x_, y, theta, 0, self.screen)
+        self.wheel4.draw(x_, y, theta, 0, self.screen)
+        draw_lines(self.map_xs, self.map_ys, self.screen, self.width, self.height)
+        if self.draw_scan_lines:
+            draw_scan_res(self.map_scan_res)
+            draw_scan_res(self.pa_scan_res)
+
+        t_end = time.time()
+        self.fps.update(1/(t_end - self.t_start))
+        str_ = f"u1: {u1:.2f}, u2: {np.rad2deg(u2):.2f}, pa: {self.car_pa_overlap:.2f}, fps: {self.fps.get():.2f}"
+        text_surface = self.my_font.render(str_, True, BLACK)
+        self.screen.blit(text_surface, (50,50))
+
+        pygame.display.update()
 
 #@numba.njit
 def model(x, u1, u2, dt, L):
@@ -42,7 +130,7 @@ def model(x, u1, u2, dt, L):
 
 
 #@numba.njit
-def get_car_corners(x, y, theta):
+def get_car_corners(x, y, theta, car_width, car_height):
     h = car_height
     w = car_width
     theta0 = np.arctan2(h/2, w)   #angle at which p1 starts at
@@ -55,16 +143,16 @@ def get_car_corners(x, y, theta):
     return [p0[0]+x, p1[0]+x, p2[0]+x, p3[0]+x], [p0[1]+y, p1[1]+y, p2[1]+y, p3[1]+y]
 
 
-def draw_car(car_xs, car_ys):
+def draw_car(car_xs, car_ys, screen, screen_width, screen_height):
     p0 = (car_xs[0], car_ys[0])
     p1 = (car_xs[1], car_ys[1])
     p2 = (car_xs[2], car_ys[2])
     p3 = (car_xs[3], car_ys[3])
 
-    p0 = pos_to_pix(*p0, width, height)
-    p1 = pos_to_pix(*p1, width, height)
-    p2 = pos_to_pix(*p2, width, height)
-    p3 = pos_to_pix(*p3, width, height)
+    p0 = pos_to_pix(*p0, screen_width, screen_height)
+    p1 = pos_to_pix(*p1, screen_width, screen_height)
+    p2 = pos_to_pix(*p2, screen_width, screen_height)
+    p3 = pos_to_pix(*p3, screen_width, screen_height)
 
     pygame.draw.polygon(screen, BLUE, (p0, p1, p2, p3))
 
@@ -78,14 +166,14 @@ def get_car_parking_area_overlap(pa_xs, pa_ys, car_xs, car_ys):
 
     return 0
 
-def draw_line(x0, y0, x1, y1):
-    p0 = pos_to_pix(x0, y0, width, height)
-    p1 = pos_to_pix(x1, y1, width, height)
+def draw_line(x0, y0, x1, y1, screen, screen_width, screen_height):
+    p0 = pos_to_pix(x0, y0, screen_width, screen_height)
+    p1 = pos_to_pix(x1, y1, screen_width, screen_height)
     pygame.draw.line(screen, BLACK, p0, p1)
 
-def draw_lines(xs, ys):
+def draw_lines(xs, ys, screen, screen_width, screen_height):
     for i in range(len(xs)-1):
-        draw_line(xs[i], ys[i], xs[i+1], ys[i+1])
+        draw_line(xs[i], ys[i], xs[i+1], ys[i+1], screen, screen_width, screen_height)
 
 @numba.njit
 def scan_numba(scan_n, car_theta, car_x, car_y, obstacle_xs, obstacle_ys):
@@ -140,114 +228,41 @@ def clean_pa_scan_lines(pa_scan, obstacle_scan):
 
 
 
-# width, height = (1280, 1280)
-width, height = (720, 720)
-screen = pygame.display.set_mode((width,height))
-pygame.font.init()
-my_font = pygame.font.SysFont(None, 30)
-fps = RollingAverage(100)
-t_start = time.time()
-draw_scan_lines = False
+if __name__ == '__main__':
+    env = ParkingSimulator()
+    env.reset()
+    u1, u2 = 0, 0
 
+    while True:
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                import sys
+                sys.exit(0)
+            elif events.type == pygame.KEYDOWN:
+                if events.dict['unicode'] == 'w':
+                    u1 = 5
+                elif events.dict['unicode'] == 'a':
+                    u2 = np.deg2rad(30)
+                elif events.dict['unicode'] == 'd':
+                    u2 = np.deg2rad(-30)
+                elif events.dict['unicode'] == 's':
+                    u1 = -5
+                elif events.dict['unicode'] == '\x1b': # esc
+                    exit(0)
+                elif events.dict['unicode'] == '\x1b': # esc
+                    exit(0)
+                elif events.dict['unicode'] == ' ':
+                    env.reset()
+            elif events.type == pygame.KEYUP:
+                if events.dict['unicode'] == 'w':
+                    u1 = 0
+                elif events.dict['unicode'] == 'a':
+                    u2 = 0
+                elif events.dict['unicode'] == 'd':
+                    u2 = 0
+                elif events.dict['unicode'] == 's':
+                    u1 = 0
 
-x = np.zeros((4, 1))
-u1 = 0
-u2 = 0
-dt = 0.5
-L = 80
-car_width = L
-car_height = 50
-car_xs , car_ys = get_car_corners(x[0, 0], x[1, 0], x[2, 0])
-car_xs.append(car_xs[0]), car_ys.append(car_ys[0]) # Make the polygon closed
-
-wheel_width = 30
-wheel_height = 10
-wheel1 = Wheel(wheel_width, wheel_height, car_width - wheel_width/2 - 5, -car_height/2 + wheel_height/2 + 5)
-wheel2 = Wheel(wheel_width, wheel_height, car_width - wheel_width/2 - 5, car_height/2 - wheel_height/2 - 5)
-wheel3 = Wheel(wheel_width, wheel_height, wheel_width/2 + 5, car_height/2 - wheel_height/2 - 5)
-wheel4 = Wheel(wheel_width, wheel_height, wheel_width/2 + 5, -car_height/2 + wheel_height/2 + 5)
-
-pa = ParkingArea([100, 300, 300, 100, 100], [100, 100, 300, 300, 100])
-
-map_xs = [-350, 350, 350, 50, 50, -100, -100, -350, -350]
-map_ys  = [-350, -350, 350, 350, 100, 100, 350, 350, -350]
-
-
-while True:
-    t_start = time.time()
-
-    screen.fill(WHITE)
-    pa.draw(screen)
-
-    x_ = x[0, 0]
-    y = x[1, 0]
-    theta = x[2, 0]
-    phi = x[3, 0]
-
-    map_scan_res = scan_numba(361, theta, x_, y, map_xs, map_ys)
-    pa_scan_res = scan_numba(361, theta, x_, y, pa.xs, pa.ys)
-    clean_pa_scan_lines(pa_scan_res, map_scan_res)
-
-    # Draw stuff
-    draw_car(car_xs[:-1], car_ys[:-1])
-    wheel1.draw(x_, y, theta, phi, screen)
-    wheel2.draw(x_, y, theta, phi, screen)
-    wheel3.draw(x_, y, theta, 0, screen)
-    wheel4.draw(x_, y, theta, 0, screen)
-    draw_lines(map_xs, map_ys)
-    if draw_scan_lines:
-        draw_scan_res(map_scan_res)
-        draw_scan_res(pa_scan_res)
-
-
-    car_pa_overlap = get_car_parking_area_overlap(pa.xs, pa.ys, car_xs, car_ys)
-    
-    text_surface = my_font.render(f"u1: {u1:.2f}, u2: {np.rad2deg(u2):.2f}, fps: {fps.get():.2f}, pa: {car_pa_overlap:.2f}", True, BLACK)
-    screen.blit(text_surface, (50,50))
-
-    pygame.display.update()
-
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            import sys
-            sys.exit(0)
-        elif events.type == pygame.KEYDOWN:
-            if events.dict['unicode'] == 'w':
-                u1 = 5
-            elif events.dict['unicode'] == 'a':
-                u2 = np.deg2rad(30)
-            elif events.dict['unicode'] == 'd':
-                u2 = np.deg2rad(-30)
-            elif events.dict['unicode'] == 's':
-                u1 = -5
-            elif events.dict['unicode'] == '\x1b': # esc
-                exit(0)
-            elif events.dict['unicode'] == '\x1b': # esc
-                exit(0)
-            elif events.dict['unicode'] == ' ':
-                x = np.zeros((4, 1))
-        elif events.type == pygame.KEYUP:
-            if events.dict['unicode'] == 'w':
-                u1 = 0
-            elif events.dict['unicode'] == 'a':
-                u2 = 0
-            elif events.dict['unicode'] == 'd':
-                u2 = 0
-            elif events.dict['unicode'] == 's':
-                u1 = 0
-
-
-    # Update car state and check for collision
-    x_temp = model(x, u1, u2, dt, L)
-    car_xs_temp, car_ys_temp = get_car_corners(x_temp[0, 0], x_temp[1, 0], x_temp[2, 0])
-    car_xs_temp.append(car_xs_temp[0]), car_ys_temp.append(car_ys_temp[0]) # Make the polygon closed
-    if ([], []) == get_polygon_intersection_points(car_xs_temp, car_ys_temp, map_xs, map_ys):
-        x = x_temp
-        car_xs, car_ys = car_xs_temp, car_ys_temp
-    else:
-        oaietnhoian =5
-
-    t_end = time.time()
-    fps.update(1/(t_end - t_start))
-
+        env.step(u1, u2)
+        env.render()
 
